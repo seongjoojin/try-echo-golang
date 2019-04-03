1e43be33a9368f4d3314ce61ef8ddfb3ca16acfc
Seongjoo Jin <evanjin8@gmail.com>
2019-04-04 08:49:27 +0900

    curd 예제 연습 중

---
+++ crud.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type (
+	user struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
+)
+
+var (
+	users = map[int]*user{}
+	seq   = 1
+)
+
+// handlers
+
+func createUser(c echo.Context) error {
+	u := &user{
+		ID: seq,
+	}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	users[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+func main() {
+
+}
